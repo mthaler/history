22d851074346a42b95ad2601819c3c39765ff2f6
Michael Thaler <michael.thaler@dlr.de>
2021-03-09 12:36:58 +0100

    Added isEmpty and currentSize tests to HistoryTest.

--- src/main/kotlin/com/mthaler/history/History.kt
+++ src/main/kotlin/com/mthaler/history/History.kt
@@ -38,7 +38,34 @@ class History<T> {
         }
     }
 
+    /**
+     * Current element or null if the history is empty
+     */
     fun current(): T? {
         TODO("Not yet implemented")
+    }
+
+    /**
+     * Returns a boolean indicating if the history is empty
+     *
+     * @return boolean indicating if history is empty
+     */
+    fun isEmpty(): Boolean = position == 0
+
+    /**
+     * Current size of the history
+     */
+    val currentSize: Int
+        get() = position
+
+    companion object {
+
+        operator fun <T>invoke(vararg elements: T): History<T> {
+            val history = History<T>()
+            for (elem in elements) {
+                history.add(elem)
+            }
+            return history
+        }
     }
 }
--- src/test/kotlin/com/mthaler/history/HistoryTest.kt
+++ src/test/kotlin/com/mthaler/history/HistoryTest.kt
@@ -1,4 +1,19 @@
 package com.mthaler.history
 
-class HistoryTest {
-}
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class HistoryTest: StringSpec({
+
+    "isEmpty" {
+        History<String>().isEmpty() shouldBe true
+        History("a").isEmpty() shouldBe false
+        History("a", "b", "c").isEmpty() shouldBe false
+    }
+
+    "currentSize" {
+        History<String>().currentSize shouldBe 0
+        History("a").currentSize shouldBe 1
+        History("a", "b", "c").currentSize shouldBe 3
+    }
+})
