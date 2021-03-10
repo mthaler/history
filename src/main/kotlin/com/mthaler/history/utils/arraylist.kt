266e5d437bbd66746dc6638de4ac73cba99f6bbb
Michael Thaler <michael.thaler@dlr.de>
2021-03-10 09:02:32 +0100

    Added removeAtStart ArrayList extension method.

--- src/main/kotlin/com/mthaler/history/utils/arraylist.kt
+++ src/main/kotlin/com/mthaler/history/utils/arraylist.kt
@@ -1,5 +1,7 @@
 package com.mthaler.history.utils
 
+import kotlin.math.min
+
 /**
  * Truncates an array list to n elements by removing elements at the end of the list
  * If the new size is >= the old size the array list will not be changed
@@ -11,4 +13,12 @@ fun <T> ArrayList<T>.truncate(newSize: Int) {
     if (oldSize > newSize) {
         this.subList(newSize, oldSize).clear()
     }
+}
+
+/**
+ * Removes n elements at the start of the list
+ */
+fun <T> ArrayList<T>.removeAtStart(n: Int) {
+    val to = min(n, size)
+    this.subList(0, to).clear()
 }
--- src/test/kotlin/com/mthaler/history/util/ArrayListTest.kt
+++ src/test/kotlin/com/mthaler/history/util/ArrayListTest.kt
@@ -1,10 +1,12 @@
 package com.mthaler.history.util
 
+import com.mthaler.history.utils.removeAtStart
 import com.mthaler.history.utils.truncate
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ArrayListTest: StringSpec({
+
     "truncate" {
         val l0 = ArrayList<String>()
         l0.truncate(5)
@@ -18,5 +20,23 @@ class ArrayListTest: StringSpec({
         val l3 = arrayListOf("a", "b", "c", "d", "e")
         l3.truncate(7)
         l3 shouldBe arrayListOf("a", "b", "c", "d", "e")
+    }
+
+    "removeAtStart" {
+        val l0 = ArrayList<String>()
+        l0.removeAtStart(5)
+        l0 shouldBe ArrayList<String>()
+        val l1 = arrayListOf("a", "b", "c", "d", "e")
+        l1.removeAtStart(2)
+        l1 shouldBe arrayListOf("c", "d", "e")
+        val l2 = arrayListOf("a", "b", "c", "d", "e")
+        l2.removeAtStart(5)
+        l2 shouldBe ArrayList<String>()
+        val l3 = arrayListOf("a", "b", "c", "d", "e")
+        l3.removeAtStart(3)
+        l3 shouldBe arrayListOf("d", "e")
+        val l4 = arrayListOf("a", "b", "c", "d", "e")
+        l4.removeAtStart(4)
+        l4 shouldBe arrayListOf("e")
     }
 })
